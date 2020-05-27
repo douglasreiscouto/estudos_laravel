df9cc3d53ecaea61206eb2b874fbcf68c1d5fe9d
Douglas-Reis <douglascouto39@gmail.com>
2020-05-26 22:52:38 -0300

    estudos sobre rotas concluido

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Facade\FlareClient\View;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +14,97 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/login', function () {
+    return "Login";
+})->name('login');
+//group para o middleware auth
+/*Route::middleware([])->group(function () {
+    //group para prefixo o prefixo admin
+    Route::prefix('admin')->group(function () {
+        //group para o namespace admin
+        Route::namespace('admin')->group(function () {
+            //group para definir o prefixo admin.
+            Route::name('admin.')->group(function () {
+                Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+
+                Route::get('/financeiro', 'TesteController@teste')->name('fincanceiro');
+
+                Route::get('/produtos', 'TesteController@teste')->name('produtos');
+
+                Route::get('/', function () {
+                    return redirect()->route('admin.dashboard');
+                })->name('home');
+            });
+        });
+    });
+});*/
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+], function () {
+    Route::get('/dashboard', 'TesteController@teste')->name('admin.dashboard');
+
+    Route::get('/financeiro', 'TesteController@teste')->name('admin.fincanceiro');
+
+    Route::get('/produtos', 'TesteController@teste')->name('admin.produtos');
+
+    Route::get('/', function () {
+        return redirect()->route('admin.dashboard');
+    })->name('home');
+});
+
+Route::get('redirect4', function () {
+    return redirect()->route('url.name');
+});
+//rotas nomeadas
+Route::get('/name-url', function () {
+    return "Hey";
+})->name('url.name');
+//redirecionamento sem funcao
+Route::redirect('redirect1', 'redirect2');
+//redirecionamento com funcao de callback
+Route::get('redirect3', function () {
+    return redirect('/redirect2');
+});
+
+Route::get('redirect2', function () {
+    return 'Recebe os redirecionamentos';
+});
+//rota com parametro dinamico
+Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
+    return "Produtos(s) {$idProduct}";
+});
+//rota com parametro e prefixo
+Route::get('/categoria/{flag}/posts', function ($flag) {
+    return "Posts da categoria: {$flag}";
+});
+//rota com parametro
+Route::get('/categorias/{flag}', function ($flag) {
+    return "Produtos da categoria: {$flag}";
+});
+//match define qual metodo que pode acessa essa rota
+Route::match(['get', 'post'], '/match', function () {
+    return 'Match';
+});
+//any aceita todos os tipos de routas ou requisições http
+Route::any('/any', function () {
+    return 'Any';
+});
+//post usado para cadastro de registro
+Route::post('/registro', function () {
+    return '';
+});
+//get usado para buscar informações
+Route::get('/empresa', function () {
+    return view('site.company');
+});
+
+Route::get('/contato', function () {
+    return view('site.contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
